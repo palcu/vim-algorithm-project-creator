35d6944fb9c6781f014a89d92641c9141d064f9e
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-03 15:02:59 +0200

    Update default cpp for 2016

--- default/default.cpp
+++ default/default.cpp
@@ -1,12 +1,10 @@
 /***************************************************
  * Alex Palcuie
- * Romania - 2015
+ * Romania - 2016
  * alex [dot] palcuie [at] gmail [dot] com
  * http://blog.palcu.ro/
 ****************************************************/
 
-#include <unordered_set>
-#include <unordered_map>
 #include <algorithm>
 #include <bitset>
 #include <climits>
@@ -27,54 +25,24 @@
 #include <sstream>
 #include <stack>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-// Defines
-#define NAME(n) #n
-#define prv(v,n) dbv((v), (#v), (n))
-#define prw(v) dbw((v), (#v))
-#define F first 
-#define S second 
-#define pb push_back
-#define sz size()
-#define mp make_pair
-typedef unsigned long long ull;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-
-// Helpers
-template <typename T> inline void dbv(const T * v, const char * name, int n){
-    fprintf(stderr, "=== %s ===\n", name);
-    for(int i=0; i<n; i++)
-        cerr << v[i] << " ";
-    cerr << '\n';
-}
-template<typename T> void dbw(const std::vector<T>& t, const char * name){
-    fprintf(stderr, "=== %s ===\n", name);
-    unsigned n = t.size();
-    for(typename std::vector<T>::size_type i=0; i<n; ++i)
-        std::cerr << t[i] << ' ';
-    cerr << '\n';
-}
-
-// Structs
-
-// Constants
 const int N = 1<<10;
 const int INF = 0x3f3f3f3f;
 
-// Globals
-
-// Functions
-int main(){
+int main() {
     #ifndef ONLINE_JUDGE
     freopen("default.in", "r", stdin);
     freopen("default.out", "w", stdout);
     #endif
     
+    tuple<string, int, float> x {"a", 1, 3.14};
+    cerr << get<3>(x) << endl;
     
     return 0;
 }
